4711b2830102df4101223d8004cae02fcaa13806
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2024-02-12 15:35:48 +0000

    Add err700

--- err700/master.sh
+++ err700/master.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-module load nvhpc/23.11
+module load nvhpc/24.1
 
-pgf90 -o master.x \
-  -mp -fPIC -Minfo -gopt -Mlarge_arrays -Mlist -traceback -Mnofma -Mbyteswapio \
-  -Mbackslash -Mstack_arrays -acc=gpu -O1 -gopt -gpu=cc70,cc80 -Minfo=accel,all,ccff master.F90
+pgf90 -o master.x -acc=gpu -O0 -gopt -gpu=cc70,cc80 -Minfo=accel,all,ccff master.F90
 
 ./master.x
