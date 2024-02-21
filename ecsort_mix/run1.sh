6eac8bf6607775f07f65ba86d28598c5087329eb
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2024-02-21 15:21:24 +0000

    Add ecsort_mix test case (nvlink problem)

--- ecsort_mix/run1.sh
+++ ecsort_mix/run1.sh
@@ -10,7 +10,7 @@ FC=pgf90
 
 \rm -f libfiat*
 
-$FC -fPIC -gopt -acc=gpu -O1 -gopt -gpu=cc70 -fPIC -c ecsort_mix.F90 
+$FC -fPIC -gopt -acc=gpu -O1 -gopt -gpu=cc70 -c ecsort_mix.F90 
 $FC -fPIC -gopt -acc=gpu -O1 -gopt -gpu=cc70 -shared -o libfiat.so ecsort_mix.o 
 $FC -acc=gpu -O1 -gopt -gpu=cc70 eloc.F90 -L. -lfiat
 
