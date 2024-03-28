ca0f3a896d045bdec0672488ba38191fb70536cc
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2024-03-28 15:59:10 +0000

    Prune

--- pbLink/test.sh
+++ pbLink/test.sh
@@ -5,12 +5,11 @@ module load nvhpc
 
 set -x
 
-
-for com in ifort pgf90
+for f90 in ifort pgf90
 do
  \rm -f *.o *.x *.mod
- $com -c geometry_mod.F90 modd_io_surf_aro.F90 write_surfl0_aro.F90
- $com -o main.x main.F90 write_surfl0_aro.o modd_io_surf_aro.o
+ $f90 -c geometry_mod.F90 modd_io_surf_aro.F90 write_surfl0_aro.F90
+ $f90 -o main.x main.F90 write_surfl0_aro.o modd_io_surf_aro.o
  \rm -f *.o *.x *.mod
 done
 
