34c6ade8b1fba3fccde14795be2178744ab55450
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2024-03-28 15:57:26 +0000

    Prune

--- pbLink/test.sh
+++ pbLink/test.sh
@@ -9,7 +9,7 @@ set -x
 for com in ifort pgf90
 do
  \rm -f *.o *.x *.mod
- $com -c type_geometry.F90 geometry_mod.F90 modd_io_surf_aro.F90 write_surfl0_aro.F90
+ $com -c geometry_mod.F90 modd_io_surf_aro.F90 write_surfl0_aro.F90
  $com -o main.x main.F90 write_surfl0_aro.o modd_io_surf_aro.o
  \rm -f *.o *.x *.mod
 done
