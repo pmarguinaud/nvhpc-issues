7205affbb559040910d0927f684ab5414d6abd4c
PENIGAUD Nicolas <penigaudn@pxalgo11>
2024-05-13 11:20:38 +0200

    add zero bit reproducibility issue

---
+++ zeroadd/launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+module load intel
+module load nvhpc
+
+set -x
+
+echo " ===   launch with nvfortran  ==="
+nvfortran -O0 -Minfo -Kieee -Mnovect -Mnofpapprox -Mnolre -Mnofprelaxed main.F90 ; ./a.out
+
+for f90 in gfortran ifort
+do
+  echo " ===   launch with $f90  ==="
+  $f90 -O3 main.F90 ; ./a.out
+done
