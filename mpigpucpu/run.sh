fc1f7e4394312c5ea814803778ca9371b180d0d3
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2024-07-26 16:29:52 +0000

    Case working/not working

--- mpigpucpu/run.sh
+++ mpigpucpu/run.sh
@@ -11,6 +11,7 @@ cd $SLURM_SUBMIT_DIR
 /home/gmap/mrpm/marguina/gpupack/support/wrap/NVHPC2405/mpif90 -acc=gpu -cuda mpl_send_mod.F90 master.F90 io_serv_send_mdl2.F90  -o master.x
 
 export MPIAUTOCONFIG=~marguina/.mpiautorc/mpiauto.PGI.conf
-~marguina/SAVE/mpiauto/mpiauto  --nouse-slurm-mpi -np 2 -- ./master.x
+~marguina/SAVE/mpiauto/mpiauto  --nouse-slurm-mpi -np 2 -- ./master.x 0
+~marguina/SAVE/mpiauto/mpiauto  --nouse-slurm-mpi -np 2 -- ./master.x 1
 
 
