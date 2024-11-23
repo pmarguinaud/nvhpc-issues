60f8f5f92e53cc4631cee91dbe1a0dcbed5c131a
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2024-11-23 16:04:56 +0000

    Create working executable/faulty executable

--- loadpack/run.sh
+++ loadpack/run.sh
@@ -5,7 +5,7 @@ module load gcc/nvhpc24.7
 set -x
 set -e
 
-\rm -f *.o *.a a.out *.mod *.lst
+\rm -f *.o *.a a.out *.mod *.lst *.x
 
 pgf90 \
  -c -mp -fPIC -Minfo -gopt -Mlarge_arrays -Mlist -traceback -Mnofma -Mbyteswapio -Mbackslash -Mstack_arrays -cuda -acc=gpu -O1 -gopt -gpu=cc70,cc80 -Minfo=accel,all,ccff \
@@ -13,8 +13,13 @@ pgf90 \
 
 pgf90 \
   -cuda -acc=gpu -O1 -gopt -gpu=cc70,cc80 \
-  master.o parkind1.o radiation_aerosol_optics_data2.o radiation_cloud_cover.o radiation_config2.o 
+  -o A.x master.o parkind1.o radiation_cloud_cover.o radiation_aerosol_optics_data2.o radiation_config2.o 
 
-./a.out
+pgf90 \
+  -cuda -acc=gpu -O1 -gopt -gpu=cc70,cc80 \
+  -o B.x master.o parkind1.o radiation_aerosol_optics_data2.o radiation_cloud_cover.o radiation_config2.o 
+
+./A.x
+./B.x
 
 
