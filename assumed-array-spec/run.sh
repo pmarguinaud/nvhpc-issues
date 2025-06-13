6571c5ba04bab82c23521a28f1a7666f46fc2c9b
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-06-13 18:23:01 +0000

    More implicit shapes

--- assumed-array-spec/run.sh
+++ assumed-array-spec/run.sh
@@ -4,8 +4,11 @@ export MODULEPATH=/ec/res4/hpcperm/sor/install/nvidia/hpc_sdk/modulefiles
 
 module load nvhpc/25.3
 
+set -x
+set -e
+
 pgf90 -mp -fPIC -Minfo -gopt -Mlarge_arrays -Mlist -traceback -Mnofma \
   -Mbyteswapio -Mbackslash -Mstack_arrays -cuda -acc=gpu -O1 -gopt -gpu=cc70,cc80 \
-  -Minfo=accel,all,ccff -o main.x main.F90 lin.F90 lin1.F90 -lnvhpcwrapnvtx -lnvToolsExt
+  -Minfo=accel,all,ccff -o main.x main.F90 lin.F90 lin1.F90 lina.F90 lin1a.F90 -lnvhpcwrapnvtx -lnvToolsExt
 
 nsys profile ./main.x
