9ffae4b0f33119f3408b29bcbd2b278b0138315a
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-08-19 08:21:22 +0000

    Add arp_ground_param

--- arp_ground_param/arp_ground_param.sh
+++ arp_ground_param/arp_ground_param.sh
@@ -2,6 +2,9 @@
 
 \rm -f a.out
 
-pgf90 -fPIC  -Mbyteswapio -Kieee -mp -Mbackslash -g -O1 -Mstack_arrays arp_ground_param.F90 
+/ec/res4/hpcperm/sor/install/nvidia/hpc_sdk/Linux_x86_64/25.7/compilers/bin/pgf90 \
+  -fPIC  -Mbyteswapio -Kieee -mp -Mbackslash -g -O1 -Mstack_arrays arp_ground_param.F90 
+
+
 ./a.out
 
