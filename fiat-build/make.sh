1666f55574a3fb4084971b375a204656c153b00d
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-02-12 13:56:25 +0000

    Try different optimization levels

--- fiat-build/make.sh
+++ fiat-build/make.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+opt=$1
+
+if [ "x$opt" != "x0" ] && [ "x$opt" != "x1" ] && [ "x$opt" != "x2" ] 
+then
+  echo "Usage: $0 0|1|2"
+  exit 1
+fi
+
 export MODULEPATH=/perm/sor/install/nvidia/hpc_sdk/modulefiles
 
 module load nvhpc-hpcx/25.7
@@ -14,10 +22,10 @@ set -e
 nvfortran \
   -I$NVHPC/Linux_x86_64/$nvhpc/comm_libs/$cuda/hpcx/hpcx-$hpcx/ompi/include \
   -I$NVHPC/Linux_x86_64/$nvhpc/comm_libs/$cuda/hpcx/hpcx-$hpcx/ompi/lib \
-  -g -O0 -Mbounds -fPIC -mp -c mpl_module.F90
+  -g -O$opt -Mbounds -fPIC -mp -c mpl_module.F90
 
 nvfortran \
-  -fPIC -g -O0 -Mbounds \
+  -fPIC -g -O$opt -Mbounds \
   -Wl,-rpath -Wl,$NVHPC/Linux_x86_64/$nvhpc/comm_libs/$cuda/hpcx/hpcx-$hpcx/ompi/lib \
   -Wl,--enable-new-dtags  -Wl,--disable-new-dtags -shared -Wl,-soname,libfiat.so -o libfiat.so mpl_module.o \
   -Wl,-rpath,"$NVHPC/Linux_x86_64/$nvhpc/comm_libs/$cuda/hpcx/hpcx-$hpcx/ompi/lib" \
@@ -30,6 +38,7 @@ nvfortran \
 nvfortran \
   -I$NVHPC/Linux_x86_64/$nvhpc/comm_libs/$cuda/hpcx/hpcx-$hpcx/ompi/include \
   -I$NVHPC/Linux_x86_64/$nvhpc/comm_libs/$cuda/hpcx/hpcx-$hpcx/ompi/lib \
-  -g -O0 -Mbounds -module . -fPIE -mp -c test_abor1.F90 
+  -g -O$opt -Mbounds -module . -fPIE -mp -c test_abor1.F90 
 
-nvfortran -Wl,--disable-new-dtags -g -O0 -Mbounds test_abor1.o -o fiat-test-abor1  -Wl,-rpath,$PWD libfiat.so 
+nvfortran -Wl,--disable-new-dtags -g -O$opt -Mbounds test_abor1.o -o fiat-test-abor1  -Wl,-rpath,$PWD libfiat.so 
+
