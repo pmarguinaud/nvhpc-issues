4de77af87ceea7ab71cdf83e69d2bd4982543e05
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-02-12 13:22:07 +0000

    Add fiat-build

---
+++ fiat-build/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+\rm -f *.so *.mod *.x *.o
---
+++ fiat-build/test_abor1.F90
@@ -0,0 +1,13 @@
+program test_abor1
+
+use mpl_module
+
+implicit none
+
+contains
+
+function mpl() result(lmpl)
+logical :: lmpl
+end function
+
+end program
